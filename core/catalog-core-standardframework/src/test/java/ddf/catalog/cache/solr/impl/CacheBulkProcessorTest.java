db4d8e19046c06ccbaa8816f9fcbb8b2420913e3
Phillip Klinefelter <phillip.klinefelter@gmail.com>
2015-05-27 11:46:07 -0700

    DDF-1213 Improved query and caching performance under high load
    - Made caching of non-cache mode queries optional and disabled it by
      default
    - Switched to a fixed size thread pool
    - Added batching to non-cache mode queries
    - Refactored CacheCommitPhaser to encapsulate its implementation
      details to match CacheBatchAdder

---
+++ core/catalog-core-standardframework/src/test/java/ddf/catalog/cache/solr/impl/CacheBulkProcessorTest.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
+ * is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+package ddf.catalog.cache.solr.impl;
+
+import static org.mockito.Matchers.anyCollectionOf;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import junit.framework.TestCase;
+
+import ddf.catalog.data.Metacard;
+import ddf.catalog.data.Result;
+
+public class CacheBulkProcessorTest extends TestCase {
+
+    private CacheBulkProcessor cacheBulkProcessor;
+
+    private SolrCache mockSolrCache;
+
+    @Override
+    public void setUp() throws Exception {
+        mockSolrCache = mock(SolrCache.class);
+        cacheBulkProcessor = new CacheBulkProcessor(mockSolrCache, 1, TimeUnit.MILLISECONDS);
+        cacheBulkProcessor.setBatchSize(10);
+        cacheBulkProcessor.setFlushInterval(10);
+    }
+
+    public void testBulkAdd() throws Exception {
+        cacheBulkProcessor.add(getMockResults(30));
+
+        while(cacheBulkProcessor.pendingMetacards() > 0) {
+            Thread.sleep(2);
+        }
+
+        verify(mockSolrCache, atLeast(3)).create(anyCollectionOf(Metacard.class));
+    }
+
+    public void testFlush() throws Exception {
+        cacheBulkProcessor.add(getMockResults(1));
+
+        while(cacheBulkProcessor.pendingMetacards() > 0) {
+            Thread.sleep(2);
+        }
+
+        verify(mockSolrCache, times(1)).create(anyCollectionOf(Metacard.class));
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        cacheBulkProcessor.shutdown();
+    }
+
+    private List<Result> getMockResults(int size) {
+        List<Result> results = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            Metacard mockMetacard = mock(Metacard.class);
+            when(mockMetacard.getId()).thenReturn(Integer.toString(i));
+
+            Result mockResult = mock(Result.class);
+            when(mockResult.getMetacard()).thenReturn(mockMetacard);
+
+            results.add(mockResult);
+        }
+
+        return results;
+    }
+
+}
