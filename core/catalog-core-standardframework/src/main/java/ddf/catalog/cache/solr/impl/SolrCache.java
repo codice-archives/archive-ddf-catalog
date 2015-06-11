cbfad397cdfa542264043ce535ff64f59ddfff38
Chris Badahdah <a.s.badahdah@gmail.com>
2015-06-11 13:24:29 -0700

    DDF-1189: reformatted code using intellij code formatter.
    Conflicts:
    	admin/module/catalog-admin-module-sources/bower.json
    	core/catalog-core-standardframework/src/main/java/ddf/catalog/cache/solr/impl/SolrCache.java

--- core/catalog-core-standardframework/src/main/java/ddf/catalog/cache/solr/impl/SolrCache.java
+++ core/catalog-core-standardframework/src/main/java/ddf/catalog/cache/solr/impl/SolrCache.java
@@ -1,17 +1,16 @@
 /**
  * Copyright (c) Codice Foundation
- *
+ * <p/>
  * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or any later version.
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. A copy of the GNU Lesser General Public License
  * is distributed along with this program and can be found at
  * <http://www.gnu.org/licenses/lgpl.html>.
- *
- **/
+ */
 package ddf.catalog.cache.solr.impl;
 
 import java.io.IOException;
@@ -339,9 +338,9 @@ public class SolrCache implements SolrCacheMBean {
 
             List<SolrQuery> sourceQueries = new ArrayList<>();
             for (String source : request.getSourceIds()) {
-                sourceQueries.add(solrFilterDelegate.propertyIsEqualTo(StringUtils
-                                        .removeEnd(METACARD_SOURCE_NAME, SchemaFields.TEXT_SUFFIX),
-                                source, true));
+                sourceQueries.add(solrFilterDelegate.propertyIsEqualTo(
+                        StringUtils.removeEnd(METACARD_SOURCE_NAME, SchemaFields.TEXT_SUFFIX),
+                        source, true));
             }
             if (sourceQueries.size() > 0) {
                 SolrQuery allSourcesQuery;
